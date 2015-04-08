7751a5e8e315693219228b0f9ad3308f7d6b0e6a
Johannes Elmnäinen <johannes.elmnainen@helsinki.fi>
2015-04-08 18:53:18 +0300

    Fix header.php and related css

--- header.php
+++ header.php
@@ -46,15 +46,8 @@
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
 				<div id="inner-header" class="wrap cf">
-
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
-
-
-					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+                                
+                                        <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 						<?php wp_nav_menu(array(
     					         'container' => false,                           // remove nav container
     					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
@@ -71,6 +64,12 @@
 
 					</nav>
 
+                                    <div id="logo">
+					<a href="<?php echo home_url(); ?>" rel="nofollow">
+                                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/logo.png">
+                                        </a>
+                                    </div>
+                                    
 				</div>
 
 			</header>
